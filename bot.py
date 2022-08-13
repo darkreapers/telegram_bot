5f590e211e2cae2a1851ae44a235b37bdffaecbd
unknown <no.8602775059@gmail.com>
2022-08-13 17:05:32 +0530

    asa

---
+++ bot.py
@@ -0,0 +1,61 @@
+#import re
+#from flask import Flask, request
+#import telegram
+#from telegram.passport.credentials import bot_token, bot_user_name,URL
+
+#imports
+import os
+import logging
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+#from wit import Wit
+
+"""bot credentials"""
+TOKEN = '5522846609:AAG4AM_8c_S7RHpruQJKbOugjUmRImb2qbU'
+PORT = int(os.environ.get('PORT', 8083))
+BOT_USER_NAME = 'Buddy1234_bot'
+URL = "https://your_project_name.herokuapp.com/"
+
+"""ai token"""
+AI_TOKEN = "access token from wit ai"
+   
+#Enable Logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+#Define few commands handlers
+def start(update, context):
+   """When /start is hit"""
+   update.message.reply_text('Hi!')
+
+def help_command(update, context):
+   """When /help is hit"""
+   update.message.reply_text('Help!')
+
+def echo(update, context):
+   """Echo the user message."""
+   update.message.reply_text('Welcome')
+   #setting wit ai
+#    ai = Wit(access_token = AI_TOKEN)
+#    resp = ai.message(update.message.text)
+
+#    print(str(resp))
+#    update.message.reply_text(str(resp))
+
+def main():
+    """starting bot"""
+    updater = Updater(TOKEN, use_context=True)
+ 
+    #getting the dispatchers to register handlers
+    dp = updater.dispatcher
+    
+    #registering commands
+    dp.add_handler(CommandHandler("start",start))
+    dp.add_handler(CommandHandler("help",help_command))
+    #registering Message Handler to reply to user
+    #starting the bot
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+   main()
