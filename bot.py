a9a54b533b3492be67472e0ecf29fe71d5fe5f84
unknown <no.8602775059@gmail.com>
2022-08-14 00:43:13 +0530

    Subjects added

--- bot.py
+++ bot.py
@@ -26,11 +26,11 @@ logger = logging.getLogger(__name__)
 #Define few commands handlers
 def start(update, context):
    """When /start is hit"""
-   update.message.reply_text('Hi!')
+   update.message.reply_text('Hello,I am buddy')
 
 def help_command(update, context):
    """When /help is hit"""
-   update.message.reply_text('Help!')
+   update.message.reply_text('use /start to start the buddy \n use /list to get list of subject of which notes are available \n To get link of any notes write /(respective no. of subject)')
 
 def subject_list(update, context):
     """When list is hit"""
@@ -47,6 +47,50 @@ def Machines(update, context):
 def Power_Electronics(update, context):
    """When /3 is hit"""
    update.message.reply_text('https://drive.google.com/file/d/1a7qqu5fIbBmLZpz9ejNefONqnIRPlFh3/view?usp=sharing')
+
+def Network(update, context):
+   """When /4 is hit"""
+   update.message.reply_text('https://drive.google.com/file/d/13DmYvq6qdGVffRFdbRu3-jnknj63p-zj/view?usp=sharing')
+
+def Control_system(update, context):
+   """When /5 is hit"""
+   update.message.reply_text('https://drive.google.com/file/d/10lWMn9aC-5WPdwIem3_SEtGbjncF0rLw/view?usp=sharing')
+
+def EMFT(update, context):
+   """When /6 is hit"""
+   update.message.reply_text('https://drive.google.com/file/d/1rt0W2yJe9Msexy7GAIay3PkWxNgKWCes/view?usp=sharing')
+
+def Signal(update, context):
+   """When /7 is hit"""
+   update.message.reply_text('https://drive.google.com/file/d/1yStT4O2BhbmMXgnL-2q7QwBmAn8_gIL6/view?usp=sharing')
+
+def Analog(update, context):
+   """When /8 is hit"""
+   update.message.reply_text('https://drive.google.com/file/d/1YVyvNBsv0BhgNU-Xt4k6Rto_xYh0APr3/view?usp=sharing')
+
+def Digital(update, context):
+   """When /9 is hit"""
+   update.message.reply_text('https://drive.google.com/file/d/1Z33DOz8T3FQXx6ded_wvE-ptCe2g9-ml/view?usp=sharing')
+
+def Measurements(update, context):
+   """When /10 is hit"""
+   update.message.reply_text('https://drive.google.com/file/d/1pr_ftuVAst08Xd8TWRR21r2b5eGG9Fkb/view?usp=sharing')
+
+def Materials(update, context):
+   """When /11 is hit"""
+   update.message.reply_text('https://drive.google.com/file/d/1OKZ9GrWkd1tX2BLD7q4mQdrDtkzlGMX9/view?usp=sharing')
+
+def Communication(update, context):
+   """When /12 is hit"""
+   update.message.reply_text('https://drive.google.com/file/d/1xT22GOTnGiU9Yoaf3SILa_piyloxnuW9/view?usp=sharing')
+
+def Microprocessor(update, context):
+   """When /13 is hit"""
+   update.message.reply_text('https://drive.google.com/file/d/1oArlhP1ghM67uR1HdFl89XOprpiBZTcr/view?usp=sharing')
+
+def Maths(update, context):
+   """When /14 is hit"""
+   update.message.reply_text('https://drive.google.com/file/d/1syJZ5KMnzqdn4e9EN35K9Ip0wbMQZJIL/view?usp=sharing')
 
 def echo(update, context):
    """Echo the user message."""
@@ -72,17 +116,17 @@ def main():
     dp.add_handler(CommandHandler("1",Power_system))
     dp.add_handler(CommandHandler("2",Machines))
     dp.add_handler(CommandHandler("3",Power_Electronics))
-    dp.add_handler(CommandHandler("4",start))
-    dp.add_handler(CommandHandler("5",start))
-    dp.add_handler(CommandHandler("6",start))
-    dp.add_handler(CommandHandler("7",start))
-    dp.add_handler(CommandHandler("8",start))
-    dp.add_handler(CommandHandler("9",start))
-    dp.add_handler(CommandHandler("10",start))
-    dp.add_handler(CommandHandler("11",start))
-    dp.add_handler(CommandHandler("12",start))
-    dp.add_handler(CommandHandler("13",start))
-    dp.add_handler(CommandHandler("14",start))
+    dp.add_handler(CommandHandler("4",Network))
+    dp.add_handler(CommandHandler("5",Control_system))
+    dp.add_handler(CommandHandler("6",EMFT))
+    dp.add_handler(CommandHandler("7",Signal))
+    dp.add_handler(CommandHandler("8",Analog))
+    dp.add_handler(CommandHandler("9",Digital))
+    dp.add_handler(CommandHandler("10",Measurements))
+    dp.add_handler(CommandHandler("11",Materials))
+    dp.add_handler(CommandHandler("12",Communication))
+    dp.add_handler(CommandHandler("13",Microprocessor))
+    dp.add_handler(CommandHandler("14",Maths))
     #registering Message Handler to reply to user
     #starting the bot
     updater.start_polling()
